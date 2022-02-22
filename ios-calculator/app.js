96dd6af355e3f72549bb04baacf0ce07927a94f8
Elif Yildiz <elifyca@gmail.com>
2022-02-21 17:48:07 -0800

    new

--- ios-calculator/app.js
+++ ios-calculator/app.js
@@ -1,183 +1,193 @@
-// DOM Elements
-const hourEl = document.querySelector('.hour');
-const minuteEl = document.querySelector('.minute');
-const valueEl = document.querySelector('.value');
+// DOM Element
 
-const acEl = document.querySelector('.ac');
-const pmEl = document.querySelector('.pm');
-const percentEl = document.querySelector('.percent');
+const ac = document.querySelector(".ac");
+const plusMinus = document.querySelector(".plus-minus");
+const percent = document.querySelector(".percent");
+const division = document.querySelector(".division");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const multiply = document.querySelector(".multiply");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const minus = document.querySelector(".minus");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const plus = document.querySelector(".plus");
+const zero = document.querySelector(".zero");
+const point = document.querySelector(".point");
+const equal = document.querySelector(".equal");
+const displayDown = document.querySelector(".display-down");
+const displayUp = document.querySelector(".display-up");
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
 
-const additionEl = document.querySelector('.addition');
-const subtractionEl = document.querySelector('.subtraction');
-const multiplicationEl = document.querySelector('.multiplication');
-const divisionEl = document.querySelector('.division');
-const equalEl = document.querySelector('.equal');
+const button = document.querySelectorAll(".button");
+const operator = document.querySelectorAll(".operator");
+const numbers = document.querySelectorAll(".number");
+const number = [zero, one, two, three, four, five, six, seven, eight, nine];
 
-const decimalEl = document.querySelector('.decimal');
-const number0El = document.querySelector('.number-0');
-const number1El = document.querySelector('.number-1');
-const number2El = document.querySelector('.number-2');
-const number3El = document.querySelector('.number-3');
-const number4El = document.querySelector('.number-4');
-const number5El = document.querySelector('.number-5');
-const number6El = document.querySelector('.number-6');
-const number7El = document.querySelector('.number-7');
-const number8El = document.querySelector('.number-8');
-const number9El = document.querySelector('.number-9');
-const numberElArray = [
-  number0El, number1El, number2El, number3El, number4El,
-  number5El, number6El, number7El, number8El, number9El
-];
+let previousOperand = "";
+let currentOperand = "";
+let operation = undefined;
+let temporaryOperand = "";
 
+// function
 
-// variables
-let valueStrInMemory = null;
-let operatorInMemory = null;
-
-
-// Functions
-const getValueAsStr = () => valueEl.textContent.split(',').join('');
-
-const getValueAsNum = () => {
-  return parseFloat(getValueAsStr());
-};
-
-const setStrAsValue = (valueStr) => {
-  if (valueStr[valueStr.length - 1] === '.') {
-    valueEl.textContent += '.';
-    return;
-  }
-
-  const [wholeNumStr, decimalStr] = valueStr.split('.');
-  if (decimalStr) {
-    valueEl.textContent =
-      parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
+function displayNumbers() {
+  if (operation) {
+    displayUp.innerHTML = `${previousOperand} ${operation}`;
   } else {
-    valueEl.textContent = parseFloat(wholeNumStr).toLocaleString();
+    displayUp.innerHTML = previousOperand;
   }
-};
-
-const handleNumberClick = (numStr) => {
-  const currentValueStr = getValueAsStr();
-  if (currentValueStr === '0') {
-    setStrAsValue(numStr);
-  } else {
-    setStrAsValue(currentValueStr + numStr);
-  }
-};
-
-const getResultOfOperationAsStr = () => {
-  const currentValueNum = getValueAsNum();
-  const valueNumInMemory = parseFloat(valueStrInMemory);
-  let newValueNum;
-  if (operatorInMemory === 'addition') {
-    newValueNum = valueNumInMemory + currentValueNum;
-  } else if (operatorInMemory === 'subtraction') {
-    newValueNum = valueNumInMemory - currentValueNum;
-  } else if (operatorInMemory === 'multiplication') {
-    newValueNum = valueNumInMemory * currentValueNum;
-  } else if (operatorInMemory === 'division') {
-    newValueNum = valueNumInMemory / currentValueNum;
-  }
-
-  return newValueNum.toString();
-};
-
-const handleOperatorClick = (operation) => {
-  const currentValueStr = getValueAsStr();
-
-  if (!valueStrInMemory) {
-    valueStrInMemory = currentValueStr;
-    operatorInMemory = operation;
-    setStrAsValue('0');
-    return;
-  }
-  valueStrInMemory = getResultOfOperationAsStr();
-  operatorInMemory = operation;
-  setStrAsValue('0');
-};
-
-
-
-
-// Add Event Listeners to functions
-acEl.addEventListener('click', () => {
-  setStrAsValue('0');
-  valueStrInMemory = null;
-  operatorInMemory = null;
-});
-pmEl.addEventListener('click', () => {
-  const currentValueNum = getValueAsNum();
-  const currentValueStr = getValueAsStr();
-
-  if (currentValueStr === '-0') {
-    setStrAsValue('0');
-    return;
-  }
-  if (currentValueNum >= 0) {
-    setStrAsValue('-' + currentValueStr);
-  } else {
-    setStrAsValue(currentValueStr.substring(1));
-  }
-});
-percentEl.addEventListener('click', () => {
-  const currentValueNum = getValueAsNum();
-  const newValueNum = currentValueNum / 100;
-  setStrAsValue(newValueNum.toString());
-  valueStrInMemory = null;
-  operatorInMemory = null;
-});
-
-
-// add event listeners to operators
-additionEl.addEventListener('click', () => {
-  handleOperatorClick('addition');
-});
-subtractionEl.addEventListener('click', () => {
-  handleOperatorClick('subtraction');
-});
-multiplicationEl.addEventListener('click', () => {
-  handleOperatorClick('multiplication');
-});
-divisionEl.addEventListener('click', () => {
-  handleOperatorClick('division');
-});
-equalEl.addEventListener('click', () => {
-  if (valueStrInMemory) {
-    setStrAsValue(getResultOfOperationAsStr());
-    valueStrInMemory = null;
-    operatorInMemory = null;
-  }
-});
-
-
-// Add Event Listeners to numbers and decimal
-for (let i=0; i < numberElArray.length; i++) {
-  const numberEl = numberElArray[i];
-  numberEl.addEventListener('click', () => {
-    handleNumberClick(i.toString());
-  });
+  displayDown.innerHTML = currentOperand;
 }
-decimalEl.addEventListener('click', () => {
-  const currentValueStr = getValueAsStr();
-  if (!currentValueStr.includes('.')) {
-    setStrAsValue(currentValueStr + '.');
+
+function appendNumber(number) {
+  // number . ile başlıyorsa ve şu anki operand . içeriyorsa yanına 2. bir . koymaması için
+  if (number === "." && currentOperand.includes(".")) return;
+  // number 0 ise ve şu anki operand 0 ise yanına başka 0 gelmemesi için
+  if (number === 0 && currentOperand === "0") return;
+  // şu anki operandın 7 basamaktan fazla olmaması için
+  if (currentOperand.length > 7) return;
+
+  if (number == multiply) {
+    displayUp.innerHTML = `${previousOperand} ${operation} ${operation}`;
   }
+
+  currentOperand = currentOperand.toString() + number.toString();
+
+  displayNumbers();
+}
+
+function chooseOperation(selectedOperation) {
+  if (temporaryOperand) {
+    previousOperand = temporaryOperand.toString();
+    currentOperand = "";
+    temporaryOperand = "";
+    operation = selectedOperation;
+    displayNumbers();
+    return;
+  }
+
+  operation = selectedOperation;
+  previousOperand = currentOperand;
+  ac.innerHTML = "AC";
+  currentOperand = "";
+
+  displayNumbers();
+}
+
+function compute() {
+  let computation;
+  const previous = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+
+  if (!operation) return;
+  if (isNaN(previous) || isNaN(current)) return;
+
+  switch (operation) {
+    case "+":
+      computation = previous + current;
+      break;
+
+    case "-":
+      computation = previous - current;
+      break;
+
+    case "÷":
+      computation = previous - current;
+      break;
+
+    case "×":
+      computation = previous * current;
+
+    default:
+      break;
+  }
+
+  if (isNaN(computation)) return;
+
+  currentOperand = computation;
+  previousOperand = "";
+  operation = undefined;
+  displayNumbers();
+  temporaryOperand = currentOperand;
+  currentOperand = "";
+}
+
+function allClear() {
+  displayUp.innerHTML = " ";
+  displayDown.innerHTML = " ";
+}
+
+function PlusMinus() {
+  currentOperand = currentOperand * -1;
+  displayNumbers();
+}
+
+function percentFunction() {
+  currentOperand = currentOperand / 100;
+  displayNumbers();
+}
+
+// Add Event Listeners
+
+operator.forEach(function (item) {
+  item.addEventListener("click", () => {
+    chooseOperation(item.innerHTML);
+  });
 });
 
+equal.addEventListener("click", () => {
+  compute();
+});
+
+ac.addEventListener("click", () => {
+  allClear();
+});
+
+plusMinus.addEventListener("click", () => {
+  PlusMinus();
+});
+
+percent.addEventListener("click", () => {
+  percentFunction();
+});
+
+numbers.forEach((item) => {
+  item.addEventListener("click", () => {
+    appendNumber(item.innerHTML);
+    temporaryOperand = "";
+  });
+});
+
+// for (let i = 0; i < number.length; i++) {
+//   const num = number[i];
+
+//   num.addEventListener("click", () => {
+//     appendNumber(i);
+//     // temporaryOperand = "";
+//   });
+// }
+
+point.addEventListener("click", () => {
+  appendNumber(".");
+});
 
 // Set up the time
+
 const updateTime = () => {
-  const currentTime = new Date();
+  let currentTime = new Date();
 
   let currentHour = currentTime.getHours();
-  const currentMinute = currentTime.getMinutes();
+  let currentMinute = currentTime.getMinutes();
 
-  if (currentHour > 12) {
-    currentHour -= 12;
-  }
-  hourEl.textContent = currentHour.toString();
-  minuteEl.textContent = currentMinute.toString().padStart(2, '0');
-}
+  hour.textContent = currentHour.toString().padStart(2, "0");
+  minute.textContent = currentMinute.toString().padStart(2, "0");
+};
+
 setInterval(updateTime, 1000);
-updateTime();
+// updateTime();
